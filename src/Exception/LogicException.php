3c73bfaabcc565126e58785f2e3f0a05822375e9
Yudedako <yudedako.red@gmail.com>
2018-07-08 17:03:07 +0900

    chore: Koriym.PhpSkeletonの導入

---
+++ src/Exception/LogicException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This file is part of the Yudedako.BlackJackPhp
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+namespace BlackJackPhp\Exception;
+
+/**
+ *  Base Logic Exception class for Yudedako.BlackJackPhp
+ */
+class LogicException extends \LogicException
+{
+}
---
+++ src/Exception/RuntimeException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This file is part of the Yudedako.BlackJackPhp
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+namespace BlackJackPhp\Exception;
+
+/**
+ *  Base Runtime Exception class for Yudedako.BlackJackPhp
+ */
+class RuntimeException extends \RuntimeException
+{
+}
